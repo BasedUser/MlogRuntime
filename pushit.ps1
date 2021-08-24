b0021552b33a76783d4fcf83f3d7bae7218c15ca
code-explorer786 <yusrilarfan2@gmail.com>
2021-08-24 17:11:38 +0700

    nvm

--- pushit.ps1
+++
@@ -1,4 +0,0 @@
-Write-Output "`e[31;1mPULLING...`e[0m"
-git pull --rebase
-Write-Output "`e[32;1mPUSHING...`e[0m"
-git push
--- pushit.sh
+++
@@ -1,4 +0,0 @@
-echo -e "\e[31;1mPULLING...\e[0m"
-git pull --rebase
-echo -e "\e[32;1mPUSHING...\e[0m"
-git push
