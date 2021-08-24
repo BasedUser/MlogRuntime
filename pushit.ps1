a612298c487b02a83069b775b15850feafc63387
code-explorer786 <yusrilarfan2@gmail.com>
2021-08-24 17:08:34 +0700

    pull&push (bash script untested)

---
+++ pushit.ps1
@@ -0,0 +1,4 @@
+Write-Output "`e[31;1mPULLING...`e[0m"
+git pull --rebase
+Write-Output "`e[32;1mPUSHING...`e[0m"
+git push
---
+++ pushit.sh
@@ -0,0 +1,4 @@
+echo -e "\e[31;1mPULLING...\e[0m"
+git pull --rebase
+echo -e "\e[32;1mPUSHING...\e[0m"
+git push
